1d378da3c5895311468e350eb2d3635156e19337
Linwenye <419766878@qq.com>
2018-05-05 01:55:43 +0800

    another easy but using long time

--- leetcode/6 zig zag/main.cpp
+++ leetcode/6 zig zag/main.cpp
@@ -5,21 +5,26 @@ using namespace std;
 class Solution {
 public:
 	string convert(string s, int numRows) {
-		string res="";
-		for (unsigned i=0;i<numRows;i++)
+		string res = "";
+		if (numRows == 1)
 		{
-			for (unsigned j=i;j<s.length();)
+			return s;
+		}
+		for (unsigned i = 0; i < numRows; i++)
+		{
+			for (unsigned j = i; j < s.length();)
 			{
 				res += s[j];
-				if (i!=0&&i!=numRows-1)
+				if (i != 0 && i != numRows - 1)
 				{
 					int next_index = j + 2 * (numRows - i - 1);
-					if (next_index<s.length())
+					if (next_index < s.length())
 					{
 						res += s[next_index];
 					}
 				}
-				j += 2 * (numRows-1);
+				// attention! need to check input 1 here! Otherwise dead loop
+				j += 2 * (numRows - 1);
 			}
 		}
 		return res;
@@ -34,4 +39,5 @@ int main() {
 	cout << res;
 	cout << endl;
 	cout << so.convert(s, 4);
+	cout << so.convert("A", 1);
 }
---
+++ leetcode/7 reverse num/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+	int reverse(int x) {
+		long long res = 0;
+		bool is_neg = x < 0;
+		unsigned int t = is_neg ? -x : x;
+		while (t != 0)
+		{
+			res = 10 * res + t % 10;
+			if (res > INT_MAX)
+			{
+				return 0;
+			}
+			t /= 10;
+		}
+
+		return int(is_neg ? -res : res);
+	}
+};
+
+int main() {
+	Solution so = Solution();
+	cout << so.reverse(1534236469);
+	cout << so.reverse(INT_MAX);
+	cout << so.reverse(INT_MIN);
+	cout << so.reverse(-10);
+}
