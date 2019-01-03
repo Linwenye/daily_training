c0684eaf87bdbe7e60ae0cc7b621b3f5c314bc9b
Linwenye <419766878@qq.com>
2019-01-03 19:39:13 +0800

    a fool can also crack this nut

---
+++ leetcode/14 Longest Common Prefix/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Solution {
+public:
+	string longestCommonPrefix(vector<string>& strs) {
+		string res = "";
+		int i = 0;
+		int max_i = strs[0].size();
+		for (auto &s : strs)
+		{
+			if (s.size() < max_i)
+			{
+				max_i = s.size();
+			}
+		}
+
+		char temp;
+		bool same = true;
+		for (; i < max_i; i++)
+		{
+			temp = strs[0][i];
+			for (auto &s : strs)
+			{
+				if (s[i] != temp)
+				{
+					same = false;
+					break;
+				}
+			}
+			if (same)
+			{
+				res += temp;
+			}
+			else
+			{
+				break;
+			}
+
+		}
+
+		return res;
+	}
+};
+
+
+int main() {
+	Solution so;
+	vector<string> strs;
+	strs.push_back("flower");
+	strs.push_back("flow");
+	cout << so.longestCommonPrefix(strs);
+}
