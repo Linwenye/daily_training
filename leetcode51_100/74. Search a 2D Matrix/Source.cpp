22ad0470623b2c6b52fb78a10a83315c9da90381
林文烨 <419766878@qq.com>
2019-04-17 21:10:33 +0800

    Update Source.cpp

--- leetcode51_100/74. Search a 2D Matrix/Source.cpp
+++ leetcode51_100/74. Search a 2D Matrix/Source.cpp
@@ -11,15 +11,21 @@ public:
 		int left = 0;
 		int right = rows * cols;
 		int mid = right / 2;
-		while (left < right) {
-			if (matrix[mid/cols][mid%cols])
+		while (left < right - 1) {
+			if (matrix[mid / cols][mid % cols] <= target)
 			{
-				
+				left = mid;
 			}
 			else
 			{
-
+				right = mid;
 			}
+			mid = (left + right) / 2;
 		}
+		if (matrix[left / cols][left % cols] == target)
+		{
+			return true;
+		}
+		else return false;
 	}
 };
